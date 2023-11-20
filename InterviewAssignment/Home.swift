d76edfb9c46c4e6f7ec72a0b6be01f3339688f93
MITHUN M R <mithunmr286@gmail.com>
2023-11-20 17:17:18 +0530

    Initial Commit
    New Project

---
+++ InterviewAssignment/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  InterviewAssignment
+//
+//  Created by MRM on 20/11/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        ScrollView {
+          
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
--- InterviewAssignment/InterviewAssignmentApp.swift
+++ InterviewAssignment/InterviewAssignmentApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct InterviewAssignmentApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Home()
         }
     }
 }
