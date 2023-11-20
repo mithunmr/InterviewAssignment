2d61155465843126d184a5bbbd5cdefac512db28
MITHUN M R <mithunmr286@gmail.com>
2023-11-20 16:55:20 +0530

    Initial Commit

---
+++ InterviewAssignment/InterviewAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewAssignmentApp.swift
+//  InterviewAssignment
+//
+//  Created by MRM on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InterviewAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
